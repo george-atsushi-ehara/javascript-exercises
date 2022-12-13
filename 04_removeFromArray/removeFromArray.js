d9e8179de6d9cbbdf8c4faf190c40d78ecb62c7b
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-13 14:07:05 -0800

    Finished 01-05::04 I had to kinda look for answer 01-03 and 05 cleared myself with Google

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removal) {
+  const removalArr = Array.from(removal);
+  const res = arr.filter(item => !removalArr.includes(item));
 
+  
+  return res;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // Make sure num1 is smaller than num2
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
 
+  // This makes a array of all the numbers between num1 and num2
+  var numArray = [];
+  for (i = num1; i <= num2; i++) {
+    numArray.push(i);
+  }
+
+  if (
+    typeof num1 !== "number" &&
+    typeof num2 !== "number" &&
+    !Array.isArray(num1) &&
+    !Array.isArray(num2)
+  ) {
+    return "ERROR";
+  } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else {
+    return (sumThis = numArray.reduce((a, b) => a + b, 0));
+  }
 };
 
 // Do not edit below this line
