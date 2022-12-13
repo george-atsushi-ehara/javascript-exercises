5ee99c113802d5c420ecd135e282d774fa78baa2
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-13 15:08:06 -0800

    07 needed help for x.0 = x and x.y = x.y, rest I got

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,34 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fc) {
+  const cel = (fc - 32) * (5 / 9);
+  // Round the number to the nearest 0.1 value
+  var rounded = Math.round(cel * 10) / 10;
+
+  // Check if the decimal part is zero
+  if (rounded % 1 === 0) {
+    // The decimal part is zero, so we can remove it
+    return Number(rounded.toFixed(0));
+  } else {
+    // The decimal part is not zero, so we keep it
+    return Number(rounded.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cf) {
+  const cel = cf * (9 / 5) + 32;
+  var rounded = Math.round(cel * 10) / 10;
+
+  // Check if the decimal part is zero
+  if (rounded % 1 === 0) {
+    // The decimal part is zero, so we can remove it
+    return Number(rounded.toFixed(0));
+  } else {
+    // The decimal part is not zero, so we keep it
+    return Number(rounded.toFixed(1));
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
