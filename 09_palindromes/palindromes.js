e1856e4c675ff8569a3b14ff01c96af240d1485c
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-14 13:00:14 -0800

    removed  punctuation and spaces

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+//   Googled how to remove all non character and spaces and found replace(/[^\w\s]/g, "") 
+//   But all the spaces were still there after console.log
+//   Googled again on how to remove spaces and found .replace(/\s/g, "")
+  let shortenedX = x
+    .replace(/[^\w\s]/g, "")
+    .replace(/\s/g, "")
+    .trim()
+    .toLowerCase();
 };
 
 // Do not edit below this line
