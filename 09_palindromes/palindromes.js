b632d18b6e881bd0017100780985bc5affacdc8f
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-14 15:03:57 -0800

    Where I stopped before asking ChatGPT

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,23 @@
 const palindromes = function (x) {
-//   Googled how to remove all non character and spaces and found replace(/[^\w\s]/g, "") 
-//   But all the spaces were still there after console.log
-//   Googled again on how to remove spaces and found .replace(/\s/g, "")
+  //   Googled how to remove all non character and spaces and found replace(/[^\w\s]/g, "")
+  //   But all the spaces were still there after console.log
+  //   Googled again on how to remove spaces and found .replace(/\s/g, "")
   let shortenedX = x
     .replace(/[^\w\s]/g, "")
     .replace(/\s/g, "")
     .trim()
     .toLowerCase();
+
+  //  Set Word length for array
+  let wordLength = x.length;
+
+  //  Check first character and last character are the same and all charcters inbetten
+  //  if character count is odd then ignore the middle character
+  for (a = 0, b = wordLength - 1; wordLength > wordLength / 2; a++, b--){
+    if (x[a] == x[b]) {
+        
+    }
+  }
 };
 
 // Do not edit below this line
