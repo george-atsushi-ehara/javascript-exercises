21e5a2900305cbe74e8fe3cf83026575226a4685
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-14 17:14:22 -0800

    mostly got it. lil mistakes like forgetting .length and not knowing .title on line 4

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (titleName) {
+  const titleArr = [];
+  for (let i = 0; i < titleName.length; i++) {
+    titleArr.push(titleName[i].title);
+  }
+  return titleArr;
 };
 
 // Do not edit below this line
