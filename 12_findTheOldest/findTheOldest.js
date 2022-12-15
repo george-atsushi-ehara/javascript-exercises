1897a59e5725f807f186936d1565cca960c3dafe
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-15 12:20:09 -0800

    First Test console shows Ray but test returns undefiend

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (nameFunction) {
+  //  Find the oldest
+  const ageArr = [];
+  for (let i = 0; i < nameFunction.length; i++) {
+    ageArr.push(nameFunction[i].yearOfDeath - nameFunction[i].yearOfBirth);
+  }
 
+  //  [New] Math.max(...arrayVariable) finds the biggest number in a array
+  console.log(nameFunction[ageArr.indexOf(Math.max(...ageArr))].name);
+  return nameFunction[ageArr.indexOf(Math.max(...ageArr))].name;
 };
 
 // Do not edit below this line
