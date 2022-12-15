2a41d571ddbe5225ceb8839aca6d01e36a45a3e6
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-14 16:48:46 -0800

    figured mostly myself

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+  const sequence = [1, 1];
+  if (x < 0) {
+    return "OOPS";
+  } else {
+    for (i = 0; i < x - 1; i++) {
+      sequence.push(sequence[i] + sequence[i + 1]);
+    }
+    return sequence[x - 1];
+  }
 };
 
 // Do not edit below this line
