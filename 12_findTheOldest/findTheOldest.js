eba5eee55d5c79694079f67e5ed24021fe2facba
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-16 16:16:38 -0800

    This is too hard. Passed first test but not 2 or 3

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,25 @@
 const findTheOldest = function (nameFunction) {
+  // Find alive
+  const eldestIfLiving = [];
   //  Find the oldest
   const ageArr = [];
-  for (let i = 0; i < nameFunction.length; i++) {
-    ageArr.push(nameFunction[i].yearOfDeath - nameFunction[i].yearOfBirth);
+
+  {
+    for (let i = 0; i < nameFunction.length; i++) {
+      // Test 1 Find the Eldest
+      ageArr.push(nameFunction[i].yearOfDeath - nameFunction[i].yearOfBirth);
+
+      // Test 2 Find Earliest BirthYear
+      eldestIfLiving.push(
+        new Date().getFullYear() - nameFunction[i].yearOfBirth
+      );
+      // if (nameFunction[i].yearOfDeath === "undefined"){
+      //   alive.push(nameFunction.name)
+    }
+    //  [New] Math.max(...arrayVariable) finds the biggest number in a array
+    return nameFunction[ageArr.indexOf(Math.max(...ageArr))];
   }
-
-  //  [New] Math.max(...arrayVariable) finds the biggest number in a array
-  console.log(nameFunction[ageArr.indexOf(Math.max(...ageArr))].name);
-  return nameFunction[ageArr.indexOf(Math.max(...ageArr))];
+  return nameFunction[eldestIfLiving.indexOf(Math.max(...eldestIfLiving))];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
