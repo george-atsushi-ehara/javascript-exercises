d1eb56a2beb21c697a15e2393d462fd5cc32f86d
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-16 16:20:32 -0800

    Never would have come to this on my own or google

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,25 +1,19 @@
-const findTheOldest = function (nameFunction) {
-  // Find alive
-  const eldestIfLiving = [];
-  //  Find the oldest
-  const ageArr = [];
-
-  {
-    for (let i = 0; i < nameFunction.length; i++) {
-      // Test 1 Find the Eldest
-      ageArr.push(nameFunction[i].yearOfDeath - nameFunction[i].yearOfBirth);
-
-      // Test 2 Find Earliest BirthYear
-      eldestIfLiving.push(
-        new Date().getFullYear() - nameFunction[i].yearOfBirth
-      );
-      // if (nameFunction[i].yearOfDeath === "undefined"){
-      //   alive.push(nameFunction.name)
-    }
-    //  [New] Math.max(...arrayVariable) finds the biggest number in a array
-    return nameFunction[ageArr.indexOf(Math.max(...ageArr))];
-  }
-  return nameFunction[eldestIfLiving.indexOf(Math.max(...eldestIfLiving))];
+const findTheOldest = function (array) {
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
-// Do not edit below this line
+
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+
 module.exports = findTheOldest;
